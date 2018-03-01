54113b0780c50d4b0b766bb392e89a778158aee3
emGiin <da_birouk@esi.dz>
2018-03-01 20:49:54 +0100

    refactored Login.js

--- tharwa-web/src/containers/Login.js
+++ tharwa-web/src/containers/Login.js
@@ -1,70 +1,92 @@
-import React, { Component } from 'react';
-import { Steps, Button, message, Icon } from 'antd';
-import './Styles/Login.css';
-import LoginForm from '../components/Login/LoginForm';
-import PinChoiceForm from '../components/Login/PinChoiceForm';
-import PinFrom from "../components/Login/PinFrom";
-const Step = Steps.Step;
+import React, { Component } from "react";
+import { Steps, Button, message, Icon } from "antd";
 
-const steps = [{
-    title: 'Login',
-    content: <LoginForm/>,
-    icon : <Icon type="user" />
-}, {
-    title: 'Code Pin',
-    content: <PinChoiceForm/>,
-    icon : <Icon type="solution" />
-}, {
-    title: 'Vérification',
-    content: <PinFrom/>,
-    icon : <Icon type="login" />
-}];
+import LoginForm from "../components/Login/LoginForm";
+import PinChoiceForm from "../components/Login/PinChoiceForm";
+import PinFrom from "../components/Login/PinFrom";
+import "./Styles/Login.css";
+
+const Step = Steps.Step;
+const steps = [
+  {
+    title: "Login",
+    content: <LoginForm />,
+    icon: <Icon type="user" />
+  },
+  {
+    title: "Code Pin",
+    content: <PinChoiceForm />,
+    icon: <Icon type="inbox" />
+  },
+  {
+    title: "Vérification",
+    content: <PinFrom />,
+    icon: <Icon type="qrcode" />
+  }
+];
 
 class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            current: 0,
-        };
-    }
-    next() {
-        const current = this.state.current + 1;
-        this.setState({ current });
-    }
-    prev() {
-        const current = this.state.current - 1;
-        this.setState({ current });
-    }
-    render() {
-        const { current } = this.state;
-        return (
-            <div className="login-part">
-                <Steps current={current}>
-                    {steps.map(item => <Step key={item.title} title={item.title} icon={item.icon}/>)}
-                </Steps>
-                <div className="steps-content">{steps[this.state.current].content}</div>
-                <div className="steps-action">
-                    {
-                        this.state.current < steps.length - 1
-                        &&
-                        <Button type="primary" onClick={() => this.next()}>Next</Button>
-                    }
-                    {
-                        this.state.current === steps.length - 1
-                        &&
-                        <Button type="primary" onClick={() => message.success('Processing complete!')}>Done</Button>
-                    }
-                    {
-                        this.state.current > 0
-                        &&
-                        <Button style={{ marginLeft: 8 }} onClick={() => this.prev()}>
-                            Previous
-                        </Button>
-                    }
-                </div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: 0
+    };
+  }
+
+  render() {
+    const current = this.state.current;
+
+    return (
+      <div className="loginForms">
+        <Steps current={current}>
+          {steps.map(item => (
+            <Step key={item.title} title={item.title} icon={item.icon} />
+          ))}
+        </Steps>
+
+        <div className="stepContent">{steps[current].content}</div>
+
+        <div className="stepActions">
+          {current > 0 && (
+            <Button onClick={this.prev.bind(this)}>
+              Previous
+            </Button>
+          )}
+          {current < steps.length - 1 && (
+            <Button type="primary" 
+              className="primaryAction"
+              onClick={this.next.bind(this)}
+            >
+              Next
+            </Button>
+          )}
+          {current === steps.length - 1 && (
+            <Button
+              type="primary"
+              className="primaryAction"     
+              onClick={this.done.bind(this)}         
+            >
+              Done
+            </Button>
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  next() {
+    const current = this.state.current + 1;
+    this.setState({ current });
+  }
+
+  prev() {
+    const current = this.state.current - 1;
+    this.setState({ current });
+  }
+
+  done() {
+   message.success("Processing complete!");  
+  }
 }
 
 export default Login;
