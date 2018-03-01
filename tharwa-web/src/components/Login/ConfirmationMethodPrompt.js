84c7854697010b07a8d4b9085446e27ea9848d47
emGiin <da_birouk@esi.dz>
2018-03-01 23:05:31 +0100

    restored ConfirmationMethodPrompt

---
+++ tharwa-web/src/components/Login/ConfirmationMethodPrompt.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Radio, Button } from "antd";
+const RadioGroup = Radio.Group;
+
+class ConfirmationMethodPrompt extends Component {
+  state = {
+    value: 1
+  };
+  onChange = e => {
+    console.log("radio checked", e.target.value);
+    this.setState({
+      value: e.target.value
+    });
+  };
+  render() {
+    return (
+      <div>
+        <p>Veuillez choisir comment vous recevez le code pin.</p>
+        <RadioGroup
+          style={{ marginTop: "3em" }}
+          onChange={this.onChange}
+          value={this.state.value}
+        >
+          <Radio value={1}>Mail</Radio>
+          <Radio value={2}>SMS</Radio>
+          {/* <Radio value={3}>J'ai déjà un code pin</Radio> */}
+        </RadioGroup>
+        <Button
+          type="primary"
+          className="primaryAction"
+          onClick={this.props.onNext}
+        >
+          Next
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default ConfirmationMethodPrompt;
