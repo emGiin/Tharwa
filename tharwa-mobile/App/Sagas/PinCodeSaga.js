01f606db59b120d45b1a1002c1b2301a841f6d23
handaoui-mohamed <handaoui.mohamed@gmail.com>
2018-03-04 21:50:36 +0100

    added pin code saga test and some minor changes

--- tharwa-mobile/App/Sagas/PinCodeSaga.js
+++ tharwa-mobile/App/Sagas/PinCodeSaga.js
@@ -7,12 +7,15 @@ export const selectPinCodeToken = (state) => state.pinCode.pinCodeToken
 // loads the login
 export function* confirmPinCode(api, { pinCode }) {
   const token = yield select(selectPinCodeToken)
-  const response = yield call(api.confirmPinCode, { pinCode, token })
+  const response = yield call(api.confirmPinCode, {
+    pin_code: pinCode,
+    temporary_token: token
+  })
 
   // success?
   if (response.ok) {
     yield put(PinCodeActions.pinCodeSuccess())
-    yield put(AuthActions.saveAuthToken(response.data.token))
+    yield put(AuthActions.saveAuthToken(response.data.token_))
   } else {
     yield put(PinCodeActions.pinCodeFailure('WRONG'))
   }
---
+++ tharwa-mobile/Tests/Sagas/PinCodeSaga.test.js
@@ -0,0 +1,76 @@
+import FixtureAPI from '../../App/Services/FixtureApi'
+import { call, put, select } from 'redux-saga/effects'
+import { confirmPinCode, selectPinCodeToken } from '../../App/Sagas/PinCodeSaga'
+import AuthActions from '../../App/Redux/AuthRedux'
+import PinCodeActions from '../../App/Redux/PinCodeRedux'
+
+const stepper = (fn) => (mock) => fn.next(mock).value
+
+describe('PIN CODE SAGA', () => {
+  it('should show pin code confirmation success path', () => {
+    const authObj = {
+      username: 'user@email.com',
+      password: 'password',
+      confirmation_method: 'sms',
+      client_id: '2',
+      grant_type: 'password',
+    }
+    const authResponse = FixtureAPI.login(authObj)
+    const pinCode = '1234'
+    const pinCodeObj = {
+      pin_code: pinCode,
+      temporary_token: authResponse.data.temporary_token
+    }
+
+    const response = FixtureAPI.confirmPinCode(pinCodeObj)
+
+    const step = stepper(confirmPinCode(FixtureAPI, { pinCode }))
+
+    expect(step(pinCodeObj.temporary_token)).toEqual(select(selectPinCodeToken))
+
+    expect(step(response)).toEqual(call(FixtureAPI.confirmPinCode, {
+      pin_code: pinCode,
+      temporary_token: response
+    }))
+    // Set the auth token on the API
+    expect(step(response)).toEqual(put(PinCodeActions.pinCodeSuccess()))
+    // Store the auth token in redux
+    expect(step(response)).toEqual(put(AuthActions.saveAuthToken(response.data.token_)))
+  })
+
+
+  it('should show pin code confirmation failure path', () => {
+    const authObj = {
+      username: 'user@email.com',
+      password: 'password',
+      confirmation_method: 'sms',
+      client_id: '2',
+      grant_type: 'password',
+    }
+    const authResponse = FixtureAPI.login(authObj)
+    const pinCode = '1235'
+    const pinCodeObj = {
+      pin_code: pinCode,
+      temporary_token: authResponse.data.temporary_token
+    }
+
+    const response = FixtureAPI.confirmPinCode(pinCodeObj)
+
+    const step = stepper(confirmPinCode(FixtureAPI, { pinCode }))
+
+    expect(step(pinCodeObj.temporary_token)).toEqual(select(selectPinCodeToken))
+
+    expect(step(response)).toEqual(call(FixtureAPI.confirmPinCode, {
+      pin_code: pinCode,
+      temporary_token: response
+    }))
+    // Set the auth token on the API
+    expect(step(response)).toEqual(put(PinCodeActions.pinCodeFailure('WRONG')))
+  })
+
+  it('should select the pin code token', () => {
+    const state = { pinCode: { pinCodeToken: 'token' } }
+    // Retrieve the API token
+    expect(selectPinCodeToken(state)).toEqual('token')
+  })
+})
