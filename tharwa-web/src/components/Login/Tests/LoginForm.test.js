2c3b076c3e89fc45b0a2f8b34288b6e05a4815ae
emGiin <da_birouk@esi.dz>
2018-03-04 21:01:27 +0100

    tested the form submission

--- tharwa-web/src/components/Login/Tests/LoginForm.test.js
+++ tharwa-web/src/components/Login/Tests/LoginForm.test.js
@@ -8,9 +8,27 @@ describe("The login form component : LoginForm", () => {
     const wrapper = shallow(<LoginForm />);
     expect(wrapper.find("NormalLoginForm")).toHaveLength(1);
   });
+  
+
+  it("should be wrapped by Form.create", ()=> {
+    // const wrapper = shallow(<LoginForm />);
+    // expect(wrapper.prop()).toHaveProperty('form');    
+  });
 
   it("should have 4 items", () => {
     const wrapper = shallow(<LoginForm />);
     expect(wrapper.dive().find("FormItem")).toHaveLength(4);
   });
+
+  it("should call onNext() after submit", () => {
+    const spy = jest.fn(); 
+    const wrapper = shallow (<LoginForm onNext={spy} />);
+    const insideWrap = wrapper.dive();
+    console.log(insideWrap.debug());
+    insideWrap.childAt(0).childAt(0).simulate('change', 'test@test.dz');
+    insideWrap.childAt(1).childAt(0).simulate('change', 'password123');
+    insideWrap.find("Form").simulate('submit', { preventDefault() {}});
+    expect(spy).toHaveBeenCalled();
+
+  })
 });
