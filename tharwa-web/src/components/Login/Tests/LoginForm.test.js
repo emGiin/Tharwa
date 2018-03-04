9d59eadf2db10b83d2c115e98abdbd11d234abb4
emGiin <da_birouk@esi.dz>
2018-03-04 08:14:55 +0100

    tested LoginForm children length

--- tharwa-web/src/components/Login/Tests/LoginForm.test.js
+++ tharwa-web/src/components/Login/Tests/LoginForm.test.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import {shallow} from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import LoginForm from '../LoginForm';
-import {Form} from 'antd';
+import LoginForm from "../LoginForm";
 
-describe('The login form component : LoginForm', ()=>{
-  it('should rendered without crashing', ()=>{
+describe("The login form component : LoginForm", () => {
+  it("should rendered without crashing", () => {
     const wrapper = shallow(<LoginForm />);
-    expect(wrapper.find('NormalLoginForm')).toHaveLength();
+    expect(wrapper.find("NormalLoginForm")).toHaveLength(1);
   });
 
-  it('should')
+  it("should have 4 items", () => {
+    const wrapper = shallow(<LoginForm />);
+    expect(wrapper.dive().find("FormItem")).toHaveLength(4);
+  });
 });
