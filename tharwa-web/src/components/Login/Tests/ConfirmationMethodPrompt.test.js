432b3e3fe751ded3289361e7f5490c8849e72797
emGiin <da_birouk@esi.dz>
2018-03-05 00:41:27 +0100

    tested ConfirmationMethodPrompt.js

--- tharwa-web/src/components/Login/Tests/ConfirmationMethodPrompt.test.js
+++ tharwa-web/src/components/Login/Tests/ConfirmationMethodPrompt.test.js
@@ -4,8 +4,36 @@ import { shallow } from "enzyme";
 import ConfirmationMethodPrompt from "../ConfirmationMethodPrompt";
 
 describe("ConfirmationMethodPromt", () => {
-  it("should rendered without crashing", () => {
-    // const wrapper = shallow(<ConfirmationMethodPrompt />);
-    // expect(wrapper.find("ConfirmationMethodPromt")).toHaveLength(1);
+  let spy;
+  let wrapper;
+
+  beforeAll(() => {
+    spy = jest.fn();
+    wrapper = shallow(<ConfirmationMethodPrompt onNext={spy} />);
+  });
+
+  it("should render without crashing", () => {
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it("should have a RadioGroup with 2 options", () => {
+    expect(wrapper.find("RadioGroup")).toHaveLength(1);
+    expect(wrapper.find("RadioGroup").find("Radio")).toHaveLength(2);
+  });
+
+  it("should have a next button", () => {
+    expect(wrapper.find("Button")).toHaveLength(1);
+  });
+
+  it("should call OnNext() after clicking Next button", () => {
+    wrapper.find("Button").simulate("click");
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it("should change state on radio button select", () => {
+    wrapper.find('RadioGroup').simulate('change', {target : {value : 1}});
+    expect(wrapper.state().choice).toEqual(1);
+    wrapper.find('RadioGroup').simulate('change', {target : {value : 2}});
+    expect(wrapper.state().choice).toEqual(2);
   });
 });
