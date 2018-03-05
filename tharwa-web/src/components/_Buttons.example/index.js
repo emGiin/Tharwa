9537b9659f97c30dfcd1fc4f99764cd0bd74c7fa
emGiin <da_birouk@esi.dz>
2018-03-05 07:54:09 +0100

    deleted examples

--- tharwa-web/src/components/_Buttons.example/index.js
+++
@@ -1,9 +0,0 @@
-import Button from "./Button.jsx";
-import ButtonDanger from './ButtonDanger.jsx';
-// or use this syntax for better imports
-// export { default } from "./ButtonDanger.jsx";
-
-export {
-  Button,
-  ButtonDanger
-}
