4190ce61749fbae68b12fabed6b5424f1c841cd0
emGiin <da_birouk@esi.dz>
2018-03-12 00:38:38 +0100

    added pinCode subreducers

--- tharwa-web/src/redux/PinCodeRedux.js
+++ tharwa-web/src/redux/PinCodeRedux.js
@@ -10,3 +10,20 @@ const { Types, Creators } = createActions({
 
 export const PinCodeTypes = Types;
 export default Creators;
+
+const INITIAL_STATE = Immutable({
+  fetching: false,
+  loading: false,
+  success: false,
+  error: null,
+  pinCodeToken: null
+});
+
+export const request = state => state.merge({ fetching: true, success: false });
+
+export const failure = (state, { error }) =>
+  state.merge({ fetching: false, error });
+export const success = state =>
+  state.merge({ fetching: false, error: null, success: true });
+
+export const save = (state, { pinCodeToken }) => state.merge({ pinCodeToken });
