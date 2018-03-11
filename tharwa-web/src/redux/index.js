b8a939050762a4d7df8cdb50e06ab66899ee2cb5
emGiin <da_birouk@esi.dz>
2018-03-12 00:39:54 +0100

    configured redux store

---
+++ tharwa-web/src/redux/index.js
@@ -0,0 +1,28 @@
+import { combineReducers } from 'redux';
+import createStore from './CreateStore'
+import rootSaga from '../sagas/'
+
+const reducers = combineReducers({
+  auth: require('./AuthRedux').reducer,
+  pinCode: require('./PinCodeRedux').reducer
+});
+
+
+export default () => {
+  let { store , sagasManager, sagaMiddleware  } = createStore(reducers, rootSaga)
+
+  if (module.hot) {
+    module.hot.accept(() => {
+      const nextRootReducer = require('./').reducers
+      store.replaceReducer(nextRootReducer)
+
+      const newYieldedSagas = require('../sagas').default
+      sagasManager.cancel()
+      sagasManager.done.then(() => {
+        sagasManager = sagaMiddleware.run(newYieldedSagas)
+      })
+    })
+  }
+
+  return store
+}
