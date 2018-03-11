5071c4a9602e64e63c8b0d80315b4cc67cd90a89
emGiin <da_birouk@esi.dz>
2018-03-12 00:37:11 +0100

    created Auth reducer

--- tharwa-web/src/redux/AuthRedux.js
+++ tharwa-web/src/redux/AuthRedux.js
@@ -1,19 +1,19 @@
-import { createReducer, createActions } from 'reduxsauce'
-import Immutable from 'seamless-immutable'
+import { createReducer, createActions } from "reduxsauce";
+import Immutable from "seamless-immutable";
 
 const { Types, Creators } = createActions({
-  authRequest: ['email', 'password', 'confirmationMethod'],
+  authRequest: ["email", "password", "confirmationMethod"],
   authSuccess: [],
-  authFailure: ['error'],
+  authFailure: ["error"],
   logoutRequest: null,
   logoutSuccess: null,
   tokenLoad: [],
   tokenLoadSuccess: [],
-  saveAuthToken: ['authToken']
-})
+  saveAuthToken: ["authToken"]
+});
 
-export const AuthTypes = Types
-export default Creators
+export const AuthTypes = Types;
+export default Creators;
 
 const INITIAL_STATE = Immutable({
   fetching: false,
@@ -21,22 +21,29 @@ const INITIAL_STATE = Immutable({
   success: false,
   error: null,
   authToken: null
-})
+});
 
-export const request = (state) => state.merge({ fetching: true, success: false })
+export const request = state => state.merge({ fetching: true, success: false });
 
-export const success = (state) => (
-  state.merge({ fetching: false, error: null, success: true })
-)
+export const success = state =>
+  state.merge({ fetching: false, error: null, success: true });
+export const failure = (state, { error }) =>
+  state.merge({ fetching: false, error });
 
-export const failure = (state, { error }) => state.merge({ fetching: false, error })
+export const load = state => state.merge({ loading: true });
+export const loadSuccess = state => state.merge({ loading: false });
+export const saveToken = (state, { authToken }) => state.merge({ authToken });
 
-// we're attempting to load token from startup sagas
-export const load = (state) => state.merge({ loading: true })
-export const loadSuccess = (state) => state.merge({ loading: false })
-export const saveToken = (state, { authToken }) => state.merge({ authToken })
+export const logoutRequest = state => state.merge({ authToken: null });
+export const logoutSuccess = state => INITIAL_STATE;
 
-// we need to logout, meaning clear access tokens and account
-export const logoutRequest = state => state.merge({ authToken: null })
-export const logoutSuccess = state => INITIAL_STATE
-
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.AUTH_REQUEST]: request,
+  [Types.AUTH_SUCCESS]: success,
+  [Types.AUTH_FAILURE]: failure,
+  [Types.TOKEN_LOAD]: load,
+  [Types.TOKEN_LOAD_SUCCESS]: loadSuccess,
+  [Types.LOGOUT_REQUEST]: logoutRequest,
+  [Types.LOGOUT_SUCCESS]: logoutSuccess,
+  [Types.SAVE_AUTH_TOKEN]: saveToken
+});
