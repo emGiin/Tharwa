50959207dc13c5e33fc6885f7884e685280eb4f0
emGiin <da_birouk@esi.dz>
2018-03-12 00:34:53 +0100

    created auth subreducers

--- tharwa-web/src/redux/AuthRedux.js
+++ tharwa-web/src/redux/AuthRedux.js
@@ -15,3 +15,28 @@ const { Types, Creators } = createActions({
 export const AuthTypes = Types
 export default Creators
 
+const INITIAL_STATE = Immutable({
+  fetching: false,
+  loading: false,
+  success: false,
+  error: null,
+  authToken: null
+})
+
+export const request = (state) => state.merge({ fetching: true, success: false })
+
+export const success = (state) => (
+  state.merge({ fetching: false, error: null, success: true })
+)
+
+export const failure = (state, { error }) => state.merge({ fetching: false, error })
+
+// we're attempting to load token from startup sagas
+export const load = (state) => state.merge({ loading: true })
+export const loadSuccess = (state) => state.merge({ loading: false })
+export const saveToken = (state, { authToken }) => state.merge({ authToken })
+
+// we need to logout, meaning clear access tokens and account
+export const logoutRequest = state => state.merge({ authToken: null })
+export const logoutSuccess = state => INITIAL_STATE
+
