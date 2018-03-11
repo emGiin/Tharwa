4f5acc3f706b469d49af9598ca9d09be36c9eba5
emGiin <da_birouk@esi.dz>
2018-03-12 00:50:52 +0100

    created Fixture API

---
+++ tharwa-web/src/services/FixtureAPI.js
@@ -0,0 +1,39 @@
+export default {
+  setAuthToken: () => {},
+  removeAuthToken: () => {},
+  login: authObj => {
+    if (
+      authObj.username === "user@email.com" &&
+      authObj.password === "password" &&
+      [1, 2].includes(authObj.confirmation_method)
+    ) {
+      return {
+        ok: true,
+        data: require("../fixtures/login.json")
+      };
+    } else {
+      return {
+        ok: false,
+        status: 400,
+        data: "Invalid credentials"
+      };
+    }
+  },
+  confirmPinCode: data => {
+    if (
+      data.pin_code === "1234" &&
+      data.temporary_token === require("../fixtures/login.json").temporary_token
+    ) {
+      return {
+        ok: true,
+        data: require("../fixtures/pinCode.json")
+      };
+    } else {
+      return {
+        ok: false,
+        status: 400,
+        data: "Invalid pin code"
+      };
+    }
+  }
+};
