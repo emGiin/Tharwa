6d40bec3cbaf27a87b68ba915e2e21fdd37a8c3a
emGiin <da_birouk@esi.dz>
2018-03-12 14:37:17 +0100

    changed configuration for api test

--- tharwa-web/src/config/AppConfig.js
+++ tharwa-web/src/config/AppConfig.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://reqres.in/api';
+export const BASE_URL = 'http://192.168.0.100:8888';
 export const AUTH_API_URL = `${BASE_URL}/oauth/`;
 export const API_URL = `${BASE_URL}/`;
