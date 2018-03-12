cd24a756ac9b075f2b9d540fa0099c3ff7fc4216
emGiin <da_birouk@esi.dz>
2018-03-12 06:03:23 +0100

    tested selectAuthToken

--- tharwa-web/src/sagas/Tests/AuthSaga.test.js
+++ tharwa-web/src/sagas/Tests/AuthSaga.test.js
@@ -36,47 +36,55 @@ describe("Authentication SAGA", () => {
     );
   });
 
-  it('should show login failure path', () => {
+  it("should show login failure path", () => {
     const authObj = {
-      username: 'use@email.com',
-      password: 'password',
+      username: "use@email.com",
+      password: "password",
       confirmation_method: 2,
-      client_id: '1',
-      grant_type: 'password',
-    }
+      client_id: "1",
+      grant_type: "password"
+    };
 
-    const response = FixtureAPI.login(authObj)
+    const response = FixtureAPI.login(authObj);
 
-    const step = stepper(login(FixtureAPI, {
-      email: 'use@email.com',
-      password: 'password',
-      confirmationMethod: 2
-    }))
+    const step = stepper(
+      login(FixtureAPI, {
+        email: "use@email.com",
+        password: "password",
+        confirmationMethod: 2
+      })
+    );
 
     // Attempt to login and fail
-    expect(step(response)).toEqual(call(FixtureAPI.login, authObj))
+    expect(step(response)).toEqual(call(FixtureAPI.login, authObj));
     // Send the error
-    expect(step(response)).toEqual(put(AuthActions.authFailure('Email ou mot de passe incorrect!')))
-  })
+    expect(step(response)).toEqual(
+      put(AuthActions.authFailure("Email ou mot de passe incorrect!"))
+    );
+  });
 
-  it('should show login load path with no token', () => {
-    const step = stepper(loadToken(FixtureAPI))
+  it("should show login load path with no token", () => {
+    const step = stepper(loadToken(FixtureAPI));
     // Attempt to select the token
-    expect(step()).toEqual(select(selectAuthToken))
+    expect(step()).toEqual(select(selectAuthToken));
     // No token was found so call success
-    expect(step()).toEqual(put(AuthActions.tokenLoadSuccess()))
-  })
+    expect(step()).toEqual(put(AuthActions.tokenLoadSuccess()));
+  });
 
-
-  it('should show login load path with a token', () => {
-    const authToken = 'sample-access-token'
-    const step = stepper(loadToken(FixtureAPI))
+  it("should show login load path with a token", () => {
+    const authToken = "sample-access-token";
+    const step = stepper(loadToken(FixtureAPI));
     // Select the token from redux and set it
-    expect(step(authToken)).toEqual(select(selectAuthToken))
-    expect(step(authToken)).toEqual(call(FixtureAPI.setAuthToken, authToken))
-    FixtureAPI.setAuthToken(authToken)
+    expect(step(authToken)).toEqual(select(selectAuthToken));
+    expect(step(authToken)).toEqual(call(FixtureAPI.setAuthToken, authToken));
+    FixtureAPI.setAuthToken(authToken);
     // The token has been set so call success
-    expect(step()).toEqual(put(AuthActions.tokenLoadSuccess()))
-  })
+    expect(step()).toEqual(put(AuthActions.tokenLoadSuccess()));
+  });
 
+  it("should select the auth token", () => {
+    const state = { auth: { authToken: "token" } };
+    // Retrieve the API token
+    expect(selectAuthToken(state)).toEqual("token");
+  });
 });
