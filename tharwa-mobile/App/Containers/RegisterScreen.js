cd5ef6efcdbcf8c9367fc9e6407755d3288b9734
handaoui-mohamed <handaoui.mohamed@gmail.com>
2018-03-12 16:57:21 +0100

    update registration screen with signup form

--- tharwa-mobile/App/Containers/RegisterScreen.js
+++ tharwa-mobile/App/Containers/RegisterScreen.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
+// import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
+import { Container } from 'native-base'
 import { connect } from 'react-redux'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
+import SignupForm from '../Components/SignupForm'
 
 // Styles
 import styles from './Styles/RegisterScreenStyle'
 
 class RegisterScreen extends Component {
-  render () {
+
+  signupFormSubmit = values => console.tron.log(values)
+
+
+  render() {
+    const { fetching } = this.props;
     return (
-      <ScrollView style={styles.container}>
-        <KeyboardAvoidingView behavior='position'>
-          <Text>RegisterScreen</Text>
-        </KeyboardAvoidingView>
-      </ScrollView>
+      <Container style={[styles.container, { paddingTop: 0 }]}>
+        {/* <Text style={styles.signupTxt}>Remplisser les champs suivants</Text> */}
+        <SignupForm
+          onSubmit={this.signupFormSubmit}
+          editable={!fetching}
+        />
+      </Container>
     )
   }
 }
 
 const mapStateToProps = (state) => {
   return {
+    fetching: false
   }
 }
 
--- tharwa-mobile/App/Containers/Styles/RegisterScreenStyle.js
+++ tharwa-mobile/App/Containers/Styles/RegisterScreenStyle.js
@@ -2,5 +2,10 @@ import { StyleSheet } from 'react-native'
 import { ApplicationStyles } from '../../Themes/'
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
+  ...ApplicationStyles.screen,
+  signupTxt: {
+    color: '#555',
+    textAlign: 'center',
+    marginVertical: 20
+  }
 })
