1e539e7abf257ca063c3a0cdb7615daab1cefc60
emGiin <da_birouk@esi.dz>
2018-03-12 14:37:03 +0100

    added empty fixtureAPI test

---
+++ tharwa-web/src/services/Tests/FixtureAPI.test.js
@@ -0,0 +1,31 @@
+import FixtureAPI from '../FixtureAPI'
+
+test('All fixtures map to actual API', () => {
+})
+
+// test('FixtureAPI getRate returns the right file', () => {
+//   const expectedFile = require('../../App/Fixtures/rateLimit.json')
+
+//   expect(FixtureAPI.getRate()).toEqual({
+//     ok: true,
+//     data: expectedFile
+//   })
+// })
+
+// test('FixtureAPI getUser returns the right file for gantman', () => {
+//   const expectedFile = require('../../App/Fixtures/gantman.json')
+
+//   expect(FixtureAPI.getUser('GantMan')).toEqual({
+//     ok: true,
+//     data: expectedFile
+//   })
+// })
+
+// test('FixtureAPI getUser returns the right file for skellock as default', () => {
+//   const expectedFile = require('../../App/Fixtures/skellock.json')
+
+//   expect(FixtureAPI.getUser('Whatever')).toEqual({
+//     ok: true,
+//     data: expectedFile
+//   })
+// })
