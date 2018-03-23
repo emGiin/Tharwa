de656006705a014a9d5970444d82eb95c9652aaf
handaoui-mohamed <handaoui.mohamed@gmail.com>
2018-03-23 15:52:10 +0100

    added input field test

--- tharwa-mobile/Tests/Components/FormFields/InputField.test.js
+++ tharwa-mobile/Tests/Components/FormFields/InputField.test.js
@@ -1,24 +1,24 @@
 import 'react-native'
 import React from 'react'
 import { shallow } from 'enzyme'
-import { EmailField } from '../../../App/Components'
+import { InputField } from '../../../App/Components'
 
-describe('Email Field Component', () => {
+describe('Input Field Component', () => {
   let wrapper, content;
 
   beforeAll(() => {
     const props = { input: {}, meta: {} }
-    wrapper = shallow(<EmailField {...props} />)
+    wrapper = shallow(<InputField {...props} />)
     content = wrapper.dive();
   })
 
-  it('should render email input', () => {
+  it('should render input', () => {
     expect(wrapper).toHaveLength(1)
     expect(content.find('Styled(Icon)')).toHaveLength(1)
     expect(content.find('Styled(Text)')).toHaveLength(0)
   })
 
-  it('should not show email errors', () => {
+  it('should not show input errors', () => {
     wrapper.setProps({ meta: { invalid: true, touched: true, error: 'error' } })
     wrapper.update()
     content = wrapper.dive();
--- tharwa-mobile/Tests/Components/FormFields/PasswordField.js
+++ tharwa-mobile/Tests/Components/FormFields/PasswordField.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { PasswordField } from '../../../App/Components'
 
-describe('LoginForm Component', () => {
+describe('Password Field Component', () => {
   let wrapper, content;
 
   beforeAll(() => {
