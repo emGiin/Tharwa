a8a1c6db8806738c8cf24066e72c192288fde76e
handaoui-mohamed <handaoui.mohamed@gmail.com>
2018-03-24 09:31:18 +0100

    added signup third step form test

---
+++ tharwa-mobile/Tests/Containers/Forms/SignupForm/ThirdStepForm.test.js
@@ -0,0 +1,33 @@
+import 'react-native'
+import React from 'react'
+import { shallow } from 'enzyme'
+import { ThirdStepForm } from '../../../../App/Containers/Forms/SignupForm/ThirdStepForm'
+
+describe('LoginForm Component', () => {
+  let container, content, instance;
+  const spy = jest.fn();
+
+  beforeAll(() => {
+    container = shallow(
+      <ThirdStepForm
+        handleSubmit={() => { }}
+        previousPage={() => { }}
+      />
+    );
+    instance = container.instance()
+    instance.focusOn = spy
+    content = container.dive()
+  })
+
+  it("should render correctly", () => {
+    expect(container).toHaveLength(1);
+    expect(content.find('Field')).toHaveLength(3)
+  });
+
+  describe('input focus', () => {
+    it('should focus on firstName', () => {
+      content.find('Field').at(0).simulate('enter')
+      expect(spy).toHaveBeenCalledWith('address')
+    })
+  })
+})
