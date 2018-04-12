70bfeebcb13fe06c7bcb71c57ef54c03c74e0f64
emGiin <da_birouk@esi.dz>
2018-04-12 20:25:46 +0100

    changed App to hold protected  routes

--- tharwa-web/src/containers/App.js
+++ tharwa-web/src/containers/App.js
@@ -1,42 +1,39 @@
 import React, { Component } from "react";
-import { Provider } from "react-redux";
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch
 } from "react-router-dom";
-import createStore from "../redux";
+import { connect } from "react-redux";
 
-import Login from "./Login";
 import ConfirmInscription from "./ConfirmInscription";
-import "./Styles/App.css";
 
-export const store = createStore();
 
 class App extends Component {
   render() {
     const isLoggedIn = (({ authToken, pinCode }) => authToken && pinCode)(
-      store.getState().auth
+      this.props.auth
     );
-
+    if (!isLoggedIn) return <Redirect to="/login" push />;
     return (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-            <Switch>
-              <Route path="/login" exact component={Login} />
-              {(isLoggedIn)? <Redirect to="/login" />: 
-              <div className="protectedRoutes">
-                <Route path="/Inscription" exact component={ConfirmInscription} />
-                <Route path="/" exact component={ConfirmInscription} />
-              </div>}
-            </Switch>
-          </div>
-        </Router>
-      </Provider>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/" component={ConfirmInscription} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  const auth = (({ authToken, pinCode }) => ({
+    authToken,
+    pinCode
+  }))(state.auth);
+  return { auth };
+};
+
+export default connect(mapStateToProps)(App);
--- tharwa-web/src/containers/Tests/App.test.js
+++ tharwa-web/src/containers/Tests/App.test.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import configureStore from "redux-mock-store";
+
 
 import App, {store} from '../App';
 
 describe('<App>', () => {
-  let wrapper; 
+  let wrapper, store,dispatcherSpy,content; 
+  const mockStore = configureStore();
+  const initialState = {
+    auth: {
+      fetching: false,
+      error: null,
+      success: false
+    },
+    pinCode: {
+      fetching: false,
+      error: null,
+      success: false
+    }
+  };
+  beforeAll(() => {
+    store = mockStore(initialState);
+    dispatcherSpy = jest.spyOn(store, "dispatch");
+    wrapper = shallow(<App />, { context: { store } });
+    content = wrapper.dive();
+  });
 
-  beforeAll(()=> {
-    wrapper = shallow(<App/>);
-  })
 
   it('should render without crashig', ()=>{
     expect(wrapper).toHaveLength(1);
   });
 
-  it('should have a provider with redux store', ()=>{
-    let provider = wrapper.find('Provider');
-    expect(provider).toHaveLength(1);
-    expect(provider.prop('store')).toBe(store);
-  })
+  // it('should have a provider with redux store', ()=>{
+  //   let provider = wrapper.find('Provider');
+  //   expect(provider).toHaveLength(1);
+  //   expect(provider.prop('store')).toBe(store);
+  // })
 });
