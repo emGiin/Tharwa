f9dcf68c56178a8acf27d789500a64ab4472f631
emGiin <da_birouk@esi.dz>
2018-04-12 17:52:52 +0100

    restored original index

--- tharwa-web/src/index.js
+++ tharwa-web/src/index.js
@@ -5,18 +5,6 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './containers/App';
 
 
-/*
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();*/
 
-import { Provider } from 'react-redux';
-import createStore from './redux'
-import ConfirmInscription from './containers/ConfirmInscription'
-export const store = createStore();
-ReactDOM.render(
-  <Provider store={store}>
-  <div className="App">
-    <ConfirmInscription />
-  </div>
-</Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
