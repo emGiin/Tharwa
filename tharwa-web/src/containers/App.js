d12262c9540ce938ffc15b7fd7800f6ced785c67
emGiin <da_birouk@esi.dz>
2018-04-12 17:53:12 +0100

    added router

--- tharwa-web/src/containers/App.js
+++ tharwa-web/src/containers/App.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux'
-import createStore from '../redux'
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import createStore from "../redux";
 
-import Login from './Login';
-import './Styles/App.css';
+import Login from "./Login";
+import ConfirmInscription from "./ConfirmInscription";
+import "./Styles/App.css";
 
-export const store = createStore()
+export const store = createStore();
 
 class App extends Component {
   render() {
+    const isLoggedIn = (({ authToken, pinCode }) => authToken && pinCode)(
+      store.getState().auth
+    );
+
     return (
       <Provider store={store}>
-        <div className="App">
-            <Login />
-        </div>
+        <Router>
+          <div className="App">
+            <Switch>
+              <Route path="/login" exact component={Login} />
+              {(isLoggedIn)? <Redirect to="/login" />: 
+              <div className="protectedRoutes">
+                <Route path="/Inscription" exact component={ConfirmInscription} />
+                <Route path="/" exact component={ConfirmInscription} />
+              </div>}
+            </Switch>
+          </div>
+        </Router>
       </Provider>
     );
   }
