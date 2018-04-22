4386d21ddd2c9bac8f62f17641fe374e8211f1fb
emGiin <da_birouk@esi.dz>
2018-04-22 20:18:03 +0100

    refactored Modals

--- tharwa-web/src/components/Reusable Components/index.js
+++ tharwa-web/src/components/Reusable Components/index.js
@@ -1,6 +1,13 @@
-import LoadingSpinner from "./LoadingSpinner"
-import RoundedImage from "./RoundedImage"
-import NumberCard from "./NumberCard"
+import LoadingSpinner from './LoadingSpinner';
+import RoundedImage from './RoundedImage';
+import NumberCard from './NumberCard';
 import TableWithActions from './TableWithActions';
+import ModalWithActions from './ModalWithActions';
 
-export {LoadingSpinner, RoundedImage, NumberCard, TableWithActions};
+export {
+  LoadingSpinner,
+  RoundedImage,
+  NumberCard,
+  TableWithActions,
+  ModalWithActions
+};
