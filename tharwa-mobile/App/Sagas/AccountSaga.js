e54ee595a5c9950ab61f86518e62a7a7250f631d
handaoui-mohamed <handaoui.mohamed@gmail.com>
2018-04-23 07:56:59 +0100

    minor change

--- tharwa-mobile/App/Sagas/AccountSaga.js
+++ tharwa-mobile/App/Sagas/AccountSaga.js
@@ -2,7 +2,7 @@ import { call, put } from 'redux-saga/effects'
 import I18n from 'react-native-i18n'
 import AccountActions from '../Redux/AccountRedux'
 
-export function* getProfile(api, { pinCode }) {
+export function* getProfile(api) {
   const response = yield call(api.getProfile)
 
   // success? 
