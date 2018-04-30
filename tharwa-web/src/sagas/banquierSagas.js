b266cc1ac74174ecb4b86fa886d3cecbe2b990ec
emGiin <da_birouk@esi.dz>
2018-05-01 00:01:32 +0100

    removed warnings

--- tharwa-web/src/sagas/banquierSagas.js
+++ tharwa-web/src/sagas/banquierSagas.js
@@ -1,4 +1,4 @@
-import { call, put, select } from "redux-saga/effects";
+import { call, put } from "redux-saga/effects";
 import banquierActions from "../redux/banquierRedux";
 import {Modal} from 'antd';
 
