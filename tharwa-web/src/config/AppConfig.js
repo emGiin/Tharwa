f314b64ae52884f5c9a27bb623b83b2b018a0e33
emGiin <da_birouk@esi.dz>
2018-06-25 10:57:41 +0100

    removed dashboard

--- tharwa-web/src/config/AppConfig.js
+++ tharwa-web/src/config/AppConfig.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://192.168.43.100:8888';
-export const BASE_URL_API = 'http://192.168.43.100:8887';
+export const BASE_URL = 'http://192.168.1.100:8888';
+export const BASE_URL_API = 'http://192.168.1.100:8887';
 export const AUTH_API_URL = `${BASE_URL}/oauth/`;
 export const API_URL = `${BASE_URL_API}/api/`;
-export const CONTENT_BASE_URL = 'http://192.168.43.100:8887/files/';
+export const CONTENT_BASE_URL = 'http://192.168.1.100:8887/files/';
