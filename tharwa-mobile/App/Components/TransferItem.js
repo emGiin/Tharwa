343a12da960f9ca7cad21d9ed1b367246f9ae096
emGiin <da_birouk@esi.dz>
2018-06-25 03:50:32 +0100

    added comission details

--- tharwa-mobile/App/Components/TransferItem.js
+++ tharwa-mobile/App/Components/TransferItem.js
@@ -1,36 +1,56 @@
-import React from 'react'
-import { View, TouchableOpacity, Dimensions } from 'react-native'
-import { Text, Icon } from 'native-base'
-import { Circle, Rect } from 'react-native-svg'
-import styles from './Styles/TransferItemStyles'
+import React from 'react';
+import { View, TouchableOpacity, Dimensions } from 'react-native';
+import { Text, Icon } from 'native-base';
+import { Circle, Rect } from 'react-native-svg';
+import styles from './Styles/TransferItemStyles';
 import { formatMoney } from '../Transforms';
-import { ContentLoader } from './'
+import { ContentLoader } from './';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const TransferItem = ({ item }) => {
-  if (!item) return <TransferLoaderItem />
-  const [date/*, time*/] = item.created_at.split(' ')
-  const type = item.transaction_direction === 'out'
-  const color = type ? "#e74c3c" : "#218c74"
-  const icon = type ? 'arrow-up' : 'arrow-down'
+  if (!item) return <TransferLoaderItem />;
+  const [date /*, time*/] = item.created_at.split(' ');
+  const type = item.transaction_direction === 'out';
+  const color = type ? '#e74c3c' : '#218c74';
+  const arrow = type ? 'arrow-up' : 'arrow-down';
+  const icons = {
+    vir_epar: 'swap-horizontal',
+    vir_devi: 'swap-horizontal',
+    vir_cour: 'swap-horizontal',
+    vir_client: 'transfer',
+    transf: 'bank',
+    micro: 'nfc',
+    reject: 'close-box'
+  };
 
   return (
     <TouchableOpacity activeOpacity={0.8} style={styles.container}>
       <View style={styles.leftContainer}>
-        <Text style={styles.target}>{item.target}</Text>
-        <Text style={styles.date}>{date}</Text>
+        <MaterialIcon style={styles.typeIcon} size={35} color="#fff" name={icons[item.transaction_type]} />
+        <View style={styles.info}>
+          <Text style={styles.target}>{item.target}</Text>
+          <Text style={styles.date}>{date}</Text>
+        </View>
       </View>
       <View style={styles.rightContainer}>
-        <Text style={[styles.amount, { color }]}>
-          {formatMoney(item.amount)}
-        </Text>
-        <Icon style={[styles.icon, { color }]} name={icon} />
+        <View style={styles.amountContainer}>
+          <Text style={[styles.amount, { color }]}>
+            {formatMoney(item.amount - item.commission)}
+          </Text>
+          <Icon style={[styles.icon, { color }]} name={arrow} />
+        </View>
+        <View style={styles.comissionContainer}>
+          <Text style={styles.comission}>{formatMoney(item.commission)}</Text>
+          {/* <MaterialIcon style={{ color: 'salmon' }} name="bell" /> */}
+          <MaterialIcon size={20} color="salmon" name="ticket-percent" />
+        </View>
       </View>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
 const TransferLoaderItem = () => {
-  const { width } = Dimensions.get('window')
+  const { width } = Dimensions.get('window');
 
   return (
     <View style={[styles.container, styles.loaderContainer]}>
@@ -46,6 +66,6 @@ const TransferLoaderItem = () => {
         <Rect x="7" y="32" rx="4" ry="4" width="50" height="8" />
       </ContentLoader>
     </View>
-  )
-}
-export { TransferItem, TransferLoaderItem }
+  );
+};
+export { TransferItem, TransferLoaderItem };
